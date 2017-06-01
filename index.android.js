5d28afa7aaf0308c3ad10b089cfabfa9054f2a0d
Prakash Rajendran <prakash.rajendran@capgemini.com>
2017-06-01 18:18:44 +0530

    fetch the movie list from rotton tomatoes

--- index.android.js
+++ index.android.js
@@ -2,5 +2,6 @@ import { AppRegistry } from 'react-native';
 import ScrollView from './src/scrollView';
 import ListView from './src/listView';
 import FetchData from './src/fetchData';
+import MovieList from './src/movieList';
 
-AppRegistry.registerComponent('AwesomeProject', () => FetchData);
+AppRegistry.registerComponent('AwesomeProject', () => MovieList);
